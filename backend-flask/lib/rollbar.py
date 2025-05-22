a99dafa29b92810e6a8189c905434f3e0acf874d
mjolsen-sw <mjolsen.sw@gmail.com>
2025-05-22 08:52:00 -0700

    cfn for backend app user and updated rollbar to capture environment

--- backend-flask/lib/rollbar.py
+++ backend-flask/lib/rollbar.py
@@ -6,11 +6,12 @@ from flask import got_request_exception
 def configure_rollbar(app):
   """Initialize Rollbar exception tracking for the Flask app."""
   rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+  flask_env = os.getenv('FLASK_ENV')
   rollbar.init(
     # access token
     rollbar_access_token,
     # environment name
-    'production',
+    flask_env,
     # server root directory, makes tracebacks prettier
     root=os.path.dirname(os.path.realpath(__file__)),
     # flask already sets up logging
